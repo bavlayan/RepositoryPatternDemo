93f0d59d2cb37b07476397dfd20ed76f0570c88c
Batuhan AVLAYAN <bavlayan@outlook.com>
2015-12-27 22:38:18 +0200

    CodeUpdate
    CodeUpdate

---
+++ RepositoryPatterDemoCode/ConsoleApplication1/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public interface IRepository<T> : IDisposable
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        IQueryable<T> GetAll();
+        T GetById(int id);
+        void SaveChanges();
+        T GetByMyQuery(Expression<Func<T, bool>> lamdaExpression);
+    }
+}
